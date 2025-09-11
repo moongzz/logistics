04d7634284029a077edc95e1c8dbeec3e0d06953
문고은 <>
2025-09-11 16:32:05 +0900

    feat: 주문 생성 시 프로모션 적용 로직 추가

--- order/src/main/java/com/msa/fiveio/order/application/facade/OrderFacadeImpl.java
+++ order/src/main/java/com/msa/fiveio/order/application/facade/OrderFacadeImpl.java
@@ -3,6 +3,7 @@ package com.msa.fiveio.order.application.facade;
 import com.msa.fiveio.order.application.usecase.ExternalService;
 import com.msa.fiveio.order.application.usecase.OrderService;
 import com.msa.fiveio.order.infrastructure.client.dto.response.ProductResponseDto;
+import com.msa.fiveio.order.infrastructure.client.dto.response.PromotionDto;
 import com.msa.fiveio.order.model.entity.Order;
 import com.msa.fiveio.order.application.dto.request.OrderCreateRequestDto;
 import com.msa.fiveio.order.application.dto.request.OrderSearchRequestDto;
@@ -30,7 +31,10 @@ public class OrderFacadeImpl implements OrdersFacade {
     public OrderCreateResponseDto createOrder(OrderCreateRequestDto orderCreateRequestDto) {
         ProductResponseDto productResponseDto = externalService.sendProductRequest(orderCreateRequestDto);
         Order order = orderCreateRequestDto.createOrder(productResponseDto.getRequesterCompanyId());
+
+        PromotionDto promotion = externalService.getPromotion(orderCreateRequestDto.getProductId());
         Order savedOrder = orderService.createOrder(productResponseDto, order);
+        orderService.applyPromotion(order, promotion);
 
         externalService.sendDeliveryRequest(savedOrder.getOrderId(), productResponseDto, orderCreateRequestDto);
         return OrderMapper.orderIdToOrderCreateResponseDto(savedOrder);
